56095d022c9bda475d8aac27f6fde052fc218077
r-set <setrakou.r@gmail.com>
2024-05-30 15:39:57 +0300

    Added script

---
+++ Assets/EditorLogParser/Editor/BuildLogParser.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class BuildLogParser : EditorWindow
+{
+    [MenuItem("Tools/Parse Build Log")]
+    public static void ParseBuildLog()
+    {
+        string userName = Environment.UserName;
+
+        string logFilePath = $"C:/Users/{userName}/AppData/Local/Unity/Editor/Editor.log";
+		//MacOS ~/Library/Logs/Unity/Editor.log
+		//Linux ~/.config/unity3d/Editor.log
+		
+        string tempLogFilePath = Path.Combine(Application.temporaryCachePath, "Editor_temp.log");
+
+        try
+        {
+            File.Copy(logFilePath, tempLogFilePath, true);
+
+            List<(string fileName, string size, string percentage)> fileSizes = ParseEditorLog(tempLogFilePath);
+            ShowParsedData(fileSizes);
+        }
+        catch (IOException e)
+        {
+            Debug.LogError($"Failed to access Editor.log file: {e.Message}");
+        }
+        finally
+        {
+            if (File.Exists(tempLogFilePath))
+            {
+                File.Delete(tempLogFilePath);
+            }
+        }
+    }
+
+    private static List<(string fileName, string size, string percentage)> ParseEditorLog(string filePath)
+    {
+        var sizePattern = new Regex(@"\s+(\d+(?:\.\d+)?\s*(?:kb|mb))\s+(\d+(\.\d+)?%)\s+(.+)");
+        var fileSizes = new List<(string fileName, string size, string percentage)>();
+
+        var inAssetsSection = false;
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            if (inAssetsSection)
+            {
+                if (line.Contains("-------------------------------------------------------------------------------"))
+                    break;
+
+                var match = sizePattern.Match(line);
+                if (match.Success)
+                {
+                    var size = match.Groups[1].Value;
+                    var percentage = match.Groups[2].Value;
+                    var fileName = match.Groups[4].Value;
+                    fileSizes.Add((fileName, size, percentage));
+                }
+            }
+            else if (line.Contains("Used Assets and files from the Resources folder, sorted by uncompressed size:"))
+            {
+                inAssetsSection = true;
+            }
+        }
+
+        return fileSizes;
+    }
+
+    private static void ShowParsedData(List<(string fileName, string size, string percentage)> fileSizes)
+    {
+        var window = GetWindow<BuildLogParser>("Parsed Build Log Data");
+        window.fileSizes = fileSizes;
+        window.Show();
+    }
+
+    private List<(string fileName, string size, string percentage)> fileSizes;
+
+    private Vector2 scrollPosition;
+
+    private void OnGUI()
+    {
+        if (fileSizes == null || fileSizes.Count == 0)
+        {
+            EditorGUILayout.LabelField("No data to display.");
+            return;
+        }
+
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(position.width), GUILayout.Height(position.height));
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("File Name", EditorStyles.boldLabel, GUILayout.Width(position.width / 2));
+        EditorGUILayout.LabelField("Size", EditorStyles.boldLabel, GUILayout.Width(position.width / 4));
+        EditorGUILayout.LabelField("Percentage", EditorStyles.boldLabel, GUILayout.Width(position.width / 4));
+        EditorGUILayout.EndHorizontal();
+
+        foreach (var (fileName, size, percentage) in fileSizes)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(fileName, GUILayout.Width(position.width / 2));
+            EditorGUILayout.LabelField(size, GUILayout.Width(position.width / 4));
+            EditorGUILayout.LabelField(percentage, GUILayout.Width(position.width / 4));
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+}
